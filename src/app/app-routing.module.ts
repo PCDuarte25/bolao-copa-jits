f1a289f43b046a1b9290133c219bc828720049a0
PCDuarte25 <pcduarte@edu.unifor.br>
2022-11-09 00:47:43 -0300

    main: Adiciona tratamento de rotas para signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'signup', pathMatch: 'full' },
+  { path: 'signup', loadChildren: () => import('./components/signup/signup.module').then(m => m.SignupModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
